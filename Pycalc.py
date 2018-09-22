95f7c584c87e47352d54b970adf7e156608aa627
FX <fxwood2@gmail.com>
2018-09-21 22:09:51 -0700

    Removed all input validation Added operator select

--- Pycalc.py
+++ Pycalc.py
@@ -1,45 +1,23 @@
 #calculator.py
 
-#create a list of valid operators
-operators = ['+','-']
 
-#Asks user for an input number
-input_number = input("input number:")
+first_number = input("input number:")
 
-#checks whether entry is numeric
-while isnumeric(input_number) == False:
+print("Your first number is: " + str(first_number))
 
-    #When entry is not a number, prints error message
-    print("not a number")
+operator = input("""choose an operation. /n
+                    For addition type 'add' /n
+                    For subtraction type 'subtr'""")
 
-    #When entry is not a number, asks user for an input number
-    input_number = input("input number:")
 
-    #Loop breaks when input is numeric
+second_number = input("input second number")
 
-input_operator = input("input operator:")
+print("your second number is:" + str(second_number))
 
-while input_operator not in operators:
+if operator == 'add':
+    print(int(first_number) + int(second_number))
 
-    print("choose a different operator")
-
-    input_operator = input("input operator")
-
-input_second_number = input("input second number")
-
-while isnumeric(input_second_number) == False:
-
-    print("not a number")
-
-    input_second_number = input("input second number")
-
-evaluate = input("evaluate? Y/N")
-
-if evaluate == "y" or "Y":
-
-    print(input_number)
-    print(input_operator)
-    print(input_second_number)
-
+if operator == 'subtr':
+    print(int(first_number) + int(second_number))
 
 
